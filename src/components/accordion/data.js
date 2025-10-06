04f70ff05db8627fe08cc79b512dce4a05129edd
offset charly <ngoacharlescedric@gmail.com>
2025-10-06 09:38:58 +0100

    project 1 simple accordian with single and multiple selection

---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answer: "Paris"
+  },
+  {
+    id: 2,
+    question: "Who wrote 'Romeo and Juliet'?",
+    answer: "William Shakespeare"
+  },
+  {
+    id: 3,
+    question: "What is the boiling point of water in Celsius?",
+    answer: "100°C"
+  },
+  {
+    id: 4,
+    question: "Which planet is known as the Red Planet?",
+    answer: "Mars"
+  }
+];
+
+export default data;
