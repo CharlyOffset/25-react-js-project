e7a1ed825464c62f276a2557d0541eca9c935e24
offset charly <ngoacharlescedric@gmail.com>
2025-10-06 11:26:55 +0100

    Project 2 Random hex and rgb Color Generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Accordian from './components/accordion'
+import RandomColor from './components/random-color'
+
 
 function App() {
 
   return (
     <>
       <Accordian />
+
+      {/* Random color component */}
+      <RandomColor />
     </>
   )
 }
